16ef47eebcc69c819daf08983483282ebd60ce8d
Mishaelangelo <michaelangelooooooo@gmail.com>
2018-05-02 12:44:38 +0300

    button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { ButtonComponent } from './shared/button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
