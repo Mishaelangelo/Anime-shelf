ae6cf2aa14ac484eacb1b9543ce915e7e44528e8
Mishaelangelo <michaelangelooooooo@gmail.com>
2018-06-21 21:04:16 +0300

    Add anime list

--- src/app/anime/anime.component.ts
+++ src/app/anime/anime.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Anime} from '../shared/models/anime.model';
+import {AnimeService} from '../services/anime.service';
+
 
 @Component({
   selector: 'app-anime',
@@ -9,7 +11,19 @@ import {Anime} from '../shared/models/anime.model';
 export class AnimeComponent implements OnInit {
   @Input() anime: Anime;
 
-  constructor() { }
+  constructor(private animeService: AnimeService) { }
 
   ngOnInit() {}
+
+  editAnime(): void {
+    const { id } = this.anime;
+    this.animeService.editAnime(id, )
+      .subscribe(() => this.animeService.animeSink.next(null));
+  }
+
+  deleteAnime(): void {
+    const { id } = this.anime;
+    this.animeService.deleteAnime(id)
+      .subscribe(() => this.animeService.animeSink.next(null));
+  }
 }
--- src/app/services/anime.service.ts
+++ src/app/services/anime.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient ,HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
-import {Anime} from '../shared/models/anime.model';
+import { HttpClient, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Anime } from '../shared/models/anime.model';
 import 'rxjs/add/operator/map';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class AnimeService {
@@ -22,14 +22,13 @@ export class AnimeService {
     return this.http.post('http://localhost:3000/anime', anime);
   }
 
-  /* editAnime(id: number, anime: Anime) {
-     const urlParams = new HttpParams().set("id", id.toString());
-     return this.http.put('http://localhost:3000/anime', anime, { params: urlParams});
-  }*/
-
-  deletAnime(id: number){
+  editAnime(id: number, anime: Anime): Observable<any> {
     const urlParams = new HttpParams().set("id", id.toString());
-    return this.http.delete('http://localhost:3000/anime', { params: urlParams});
+     return this.http.put(`http://localhost:3000/anime/${id}`, anime, { params: urlParams});
+  }
+
+  deleteAnime(id: number): Observable<any> {
+    return this.http.delete(`http://localhost:3000/anime/${id}`);
   }
 }
 
