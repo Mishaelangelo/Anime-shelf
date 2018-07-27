168e427483ffe176542c8f83a8209618de2c4c08
Mishaelangelo <michaelangelooooooo@gmail.com>
2018-07-28 01:25:01 +0300

    made sao

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth-service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  Facebook() {
+   this.authService.loginFacebook();
+  }
+
+  Google() {
+    this.authService.loginGoogle();
+  }
 }
